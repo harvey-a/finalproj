6de5f54850cd0c7bd6ddc5711171f775b5ea9fca
Harvey <harvey@github.com>
2018-04-30 21:47:07 +0800

    initial

---
+++ mysite/app/admin/forms.py
@@ -0,0 +1,27 @@
+from wtforms import StringField, SubmitField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms.validators import DataRequired
+
+from ..models import Department, Role
+
+class DepartmentForm(FlaskForm):
+  
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class RoleForm(FlaskForm):
+   
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EmployeeAssignForm(FlaskForm):
+   
+    department = QuerySelectField(query_factory=lambda: Department.query.all(),
+                                  get_label="name")
+    role = QuerySelectField(query_factory=lambda: Role.query.all(),
+                            get_label="name")
+    submit = SubmitField('Submit')
---
+++ mysite/app/models.py
@@ -0,0 +1,52 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login_manager
+
+class Employee(UserMixin, db.Model):
+    __tablename__ = 'employees'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(60), index=True, unique=True)
+    username = db.Column(db.String(60), index=True, unique=True)
+    first_name = db.Column(db.String(60), index=True)
+    last_name = db.Column(db.String(60), index=True)
+    password_hash = db.Column(db.String(128))
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<Employee: {}>'.format(self.username)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Employee.query.get(int(user_id))
+
+
+class Department(db.Model):
+    __tablename__ = 'departments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='department')
+    def __repr__(self):
+        return '<Department: {}>'.format(self.name)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='role')
+
+    def __repr__(self):
+        return '<Role: {}>'.format(self.name)
